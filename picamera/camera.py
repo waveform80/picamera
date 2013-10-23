fbb49465dbe1e01862450d64ed496ac24cc50b60
Dave Hughes <dave@waveform.org.uk>
2013-10-24 00:20:49 +0100

    Missed an import

--- picamera/camera.py
+++ picamera/camera.py
@@ -24,7 +24,7 @@ from picamera.encoders import (
     PiVideoEncoder,
     PiImageEncoder,
     PiRawImageEncoder,
-    PiOneImageEncoder,
+    PiCookedImageEncoder,
     PiMultiImageEncoder,
     )
 
