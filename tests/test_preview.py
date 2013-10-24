30821f48849789f5bfbcfe35428051fd917e841a
Dave Hughes <dave@waveform.org.uk>
2013-10-24 15:14:19 +0100

    Added py2 compat stuff to preview tests

--- tests/test_preview.py
+++ tests/test_preview.py
@@ -1,3 +1,13 @@
+from __future__ import (
+    unicode_literals,
+    print_function,
+    division,
+    absolute_import,
+    )
+
+# Make Py2's str equivalent to Py3's
+str = type('')
+
 import picamera
 import pytest
 
