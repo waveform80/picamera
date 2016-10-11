482cfa9535e51b6af63b72cd0cea3181b95aef85
Dave Jones <dave@waveform.org.uk>
2016-10-12 00:27:58 +0100

    Require newer sphinx
    Needed on RTD for imgmath support

--- docs/conf.py
+++ docs/conf.py
@@ -69,6 +69,7 @@ sys.modules['numpy.lib.stride_tricks'] = sys.modules['numpy'].lib.stride_tricks
 
 # -- General configuration ------------------------------------------------
 
+needs_sphinx = '1.4.3'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx', 'sphinx.ext.imgmath']
 templates_path = ['_templates']
 source_suffix = '.rst'
